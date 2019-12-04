14572cb756d21d69c448cc4cd4f811dd2bd51d75
rwz657026189 <wenzhang.ren@pactera.com>
2019-12-04 18:06:30 +0800

    优化网络请求日志

--- lib_comm/src/main/java/com/rwz/lib_comm/net/interceptor/CommLoggingInterceptor.kt
+++ lib_comm/src/main/java/com/rwz/lib_comm/net/interceptor/CommLoggingInterceptor.kt
@@ -18,37 +18,33 @@ class CommLoggingInterceptor : Interceptor {
         //这个chain里面包含了request和response，所以你要什么都可以从这里拿
         val request = chain.request()
         val body = request.body()
-        LogUtil.ok(TAG, "heads = " + request.headers())
+        val sb = StringBuilder()
+        sb.append("【url】：${request.url()}").append("\n")
+        sb.append("【header】").append("\n")
+            .append("     ").append(request.headers()).append("\n")
+        sb.append("【body】").append("\n")
         if (body is FormBody) {
-            val formBody = body as FormBody?
-            val size = formBody!!.size()
-            val sb = StringBuilder()
+            val size = body.size()
             for (i in 0 until size) {
-                sb.append("║   ").append(formBody.name(i)).append(" = ").append(formBody.value(i)).append("\n")
+                sb.append("     ").append(body.name(i)).append(" = ")
+                    .append(body.value(i))
             }
-            LogUtil.ok(TAG, "url : \n║   " + request.url(), "\n║   请求body : \n$sb")
         }
-        val t1 = System.currentTimeMillis()//请求发起的时间
+        sb.append("\n")
+        LogUtil.ok(sb.toString())
+
+        val startTime = System.nanoTime()//请求发起的时间
         val response = chain.proceed(request)
-        val t2 = System.currentTimeMillis()//收到响应的时间
+        val endTime = System.nanoTime()//收到响应的时间
         //这里不能直接使用response.body().string()的方式输出日志
         //因为response.body().string()之后，response中的流会被关闭，程序会报错，我们需要创建出一
         //个新的response给应用层处理
-
         val responseBody = response.peekBody((1024 * 1024).toLong())
-
-        val url = response.request().url()
-        LogUtil.ok(
-            TAG, "接收响应: \n║   " +
-                    url + "\n║   " +
-                    "\n║   " +
-                    "响应头：" + response.headers()
-        )
-        LogUtil.ok(TAG + ", " + responseBody.string())
+        sb.clear()
+        sb.append("【url】：${response.request().url()}").append("\n")
+        sb.append("【响应时间】：${endTime - startTime}ms")
+        LogUtil.ok(sb.toString())
+        LogUtil.j(jsonStr = responseBody.string())
         return response
-    }
-
-    companion object {
-        private const val TAG = "CommLoggingInterceptor"
     }
 }
--- lib_comm/src/main/java/com/rwz/lib_comm/utils/show/LogUtil.kt
+++ lib_comm/src/main/java/com/rwz/lib_comm/utils/show/LogUtil.kt
@@ -2,8 +2,10 @@ package com.rwz.lib_comm.utils.show
 
 import android.text.TextUtils
 import android.util.Log
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import com.rwz.lib_comm.config.showLog
-import java.util.*
 
 /**
  * date： 2019/11/7 16:28
@@ -12,18 +14,28 @@ import java.util.*
  **/
 object LogUtil {
 
-    var isDebug = showLog
+    private var isDebug = showLog
 
-    private val TAG = "rwz"
-    private val OkHttp = "OkHttp"
-    private val SPACE = "   "
+    private const val TAG = "rwz"
+    private const val OkHttp = "OkHttp"
+    private const val SPACE = "   "
+
+    init {
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+            .methodCount(1)         // (Optional) How many method line to show. Default 2
+            .methodOffset(1)        // (Optional) Hides internal method calls up to offset. Default 5
+            .tag(TAG)               // (Optional) Global tag for every log. Default PRETTY_LOGGER
+            .build()
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+    }
 
     /**
      * 打印网络请求日志
      */
     fun ok(vararg msg: Any?) {
         if (isDebug) {
-            Log.d(OkHttp, msg.contentToString())
+            Logger.t(OkHttp).d(getText(msg))
         }
     }
 
@@ -32,7 +44,7 @@ object LogUtil {
      */
     fun d(vararg msg: Any?) {
         if (isDebug) {
-            Log.d(TAG, msg.contentToString())
+            Logger.t(TAG).d(getText(msg))
         }
     }
 
@@ -41,24 +53,22 @@ object LogUtil {
      */
     fun e(vararg msg: Any?) {
         if (isDebug) {
-            Log.e(TAG, msg.contentToString())
+            Logger.t(TAG).e(getText(msg))
         }
     }
 
     /**
      * 打印json
      */
-    fun j(jsonStr: String) {
+    fun j(tag: String = OkHttp, jsonStr: String) {
         if (isDebug) {
             if (TextUtils.isEmpty(jsonStr))
                 return
-
+            Logger.t(tag).json(jsonStr)
         }
     }
 
     private fun getText(vararg msg: Any): String {
-        if (msg == null)
-            return ""
         val sb = StringBuffer()
         for (o in msg) {
             sb.append(",").append(o)
@@ -66,11 +76,7 @@ object LogUtil {
         return sb.toString().substring(1)
     }
 
-    fun stackTraces() {
-        stackTraces(15, 3)
-    }
-
-    fun stackTraces(methodCount: Int, methodOffset: Int) {
+    fun stackTraces(methodCount: Int = 15, methodOffset: Int = 3) {
         val trace = Thread.currentThread().stackTrace
         var level = ""
         Log.d(TAG, "$SPACE--------- logStackTraces start ----------")

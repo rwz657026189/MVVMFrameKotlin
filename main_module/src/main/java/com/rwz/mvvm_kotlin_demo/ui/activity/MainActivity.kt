d7adf96f8e824bf898d4932e19f6c06f5d4be0b5
rwz657026189 <wenzhang.ren@pactera.com>
2020-05-12 17:15:17 +0800

    更新gradle版本，引入协程(非必须)、调整部分test依赖

--- main_module/src/main/java/com/rwz/mvvm_kotlin_demo/ui/activity/MainActivity.kt
+++ main_module/src/main/java/com/rwz/mvvm_kotlin_demo/ui/activity/MainActivity.kt
@@ -12,6 +12,7 @@ import com.rwz.lib_comm.utils.factory.FragmentFactory
 import com.rwz.mvvm_kotlin_demo.R
 import com.rwz.mvvm_kotlin_demo.ui.fragment.MainListFragment
 import com.rwz.mvvm_kotlin_demo.ui.fragment.MineFragment
+import kotlinx.coroutines.*
 import java.util.*
 
 class MainActivity : BaseTabVpActivity<ActivityTabVpBinding>() {
@@ -47,4 +48,34 @@ class MainActivity : BaseTabVpActivity<ActivityTabVpBinding>() {
             super.onBackPressed()
     }
 
+}
+
+@ExperimentalCoroutinesApi
+fun main() {
+    println("${Thread.currentThread().name} step1")
+    val job = GlobalScope.launch(Dispatchers.Unconfined) {
+        println("${Thread.currentThread().name} step2")
+        val data = requestData()
+        println("${Thread.currentThread().name} step3 $data")
+        delay(100)
+        println("${Thread.currentThread().name} step4")
+    }
+//    job.cancel()
+    println("${Thread.currentThread().name} step5")
+    Thread.sleep(3000)
+
+}
+
+suspend fun requestData() : String{
+   return withContext(Dispatchers.Default) {
+       Thread.sleep(1000)
+       "abc"
+   }
+}
+
+suspend fun requestData2() : String{
+    return GlobalScope.async {
+        Thread.sleep(1000)
+        "abc"
+    }.await()
 }

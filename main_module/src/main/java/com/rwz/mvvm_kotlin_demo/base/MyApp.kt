225b11f18f57b4937420d0da49a32ee57f923f99
rwz <18380205254@163.com>
2020-06-04 11:28:28 +0800

    分包

--- main_module/src/main/java/com/rwz/mvvm_kotlin_demo/base/MyApp.kt
+++ main_module/src/main/java/com/rwz/mvvm_kotlin_demo/base/MyApp.kt
@@ -1,6 +1,8 @@
 package com.rwz.mvvm_kotlin_demo.base
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.rwz.lib_comm.config.ONLINE_HTTP
 import com.rwz.lib_comm.manager.ContextManager
 import com.rwz.lib_comm.net.RetrofitManager
@@ -16,6 +18,11 @@ open class MyApp : Application() {
         super.onCreate()
         ContextManager.context = this
         RetrofitManager.init(ONLINE_HTTP)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 
 }

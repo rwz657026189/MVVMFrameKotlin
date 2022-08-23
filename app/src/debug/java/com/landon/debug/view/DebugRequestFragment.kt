8257f6fc41c7bd6e1ac4953f9bbfda715b5f5f8b
任文章 <18380205254@163.com>
2022-08-23 11:36:07 +0800

    fix:网络请求bug

--- app/src/debug/java/com/landon/debug/view/DebugRequestFragment.kt
+++ app/src/debug/java/com/landon/debug/view/DebugRequestFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.landon.debug.utils.LogUtil
 import com.rwz.app.R
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -40,11 +41,11 @@ class DebugRequestFragment(
     fun setNetEntity(entity: DevNetEntity?) {
         if (isAdded) {
             holder.setText(R.id.url, entity?.url)
-                    .setText(R.id.method, entity?.method)
-                    .setText(R.id.header, entity?.header)
-                    .setText(R.id.body, entity?.postForm)
-                    .setText(R.id.response, entity?.respText)
-                    .setListener(R.id.perform, this::performRequest)
+                .setText(R.id.method, entity?.method)
+                .setText(R.id.header, entity?.header)
+                .setText(R.id.body, entity?.postForm)
+                .setText(R.id.response, entity?.respText)
+                .setListener(R.id.perform, this::performRequest)
         } else {
             this.entity = entity
         }
@@ -61,33 +62,27 @@ class DebugRequestFragment(
             Toast.makeText(requireContext(), "method", Toast.LENGTH_SHORT).show()
             return
         }
-        val headers = Headers.Builder().apply {
-            holder.getText(R.id.header)?.split("&")?.forEach { params ->
-                val key = params.split("=").getOrNull(0)
-                val value = params.split("=").getOrNull(1)
-                if (key.isNullOrEmpty().not() && value != null) {
-                    add(key!!, value)
-                }
-            }
-        }.build()
-        val body = holder.getText(R.id.body)?.toRequestBody("application/json;charset=UTF-8".toMediaTypeOrNull())
+        val headers = parseHeader(holder.getText(R.id.header) ?: "")
+        val body = parseBody(holder.getText(R.id.body) ?: "")
         val timeout = holder.getText(R.id.timeout)?.toIntOrNull() ?: 60
         val okHttpClient = if (client.connectTimeoutMillis != timeout) {
             client.newBuilder()
-                    .connectTimeout(Duration.ofSeconds(timeout.toLong()))
-                    .readTimeout(Duration.ofSeconds(timeout.toLong()))
-                    .writeTimeout(Duration.ofSeconds(timeout.toLong()))
-                    .build()
+                .connectTimeout(Duration.ofSeconds(timeout.toLong()))
+                .readTimeout(Duration.ofSeconds(timeout.toLong()))
+                .writeTimeout(Duration.ofSeconds(timeout.toLong()))
+                .build()
         } else client
         val request = Request.Builder()
-                .url(url)
-                .method(method, body)
-                .headers(headers)
-                .build()
+            .url(url)
+            .method(method, body)
+            .headers(headers)
+            .build()
         okHttpClient.newCall(request).enqueue(object: Callback{
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
-                holder.setText(R.id.response, e.toString())
+                activity?.runOnUiThread {
+                    holder.setText(R.id.response, e.toString())
+                }
             }
 
             override fun onResponse(call: Call, response: Response) {
@@ -98,8 +93,52 @@ class DebugRequestFragment(
                     e.printStackTrace()
                     e.toString()
                 }
-                holder.setText(R.id.response, resp)
+                activity?.runOnUiThread {
+                    holder.setText(R.id.response, resp)
+                }
             }
         })
+    }
+
+    private fun parseBody(bodyText: String): RequestBody? {
+        var mediaType: String? = null
+        val realBodyText: String? = try {
+            val json = JSONObject(bodyText).toString()
+            mediaType = "application/json;charset=UTF-8"
+            json
+        } catch (e: Exception) {
+            if (bodyText.contains("&")) {
+                mediaType = "application/x-www-form-urlencoded"
+                if (bodyText.endsWith("&")) bodyText.subSequence(0, bodyText.length - 1).toString() else bodyText
+            } else {
+                null
+            }
+        }
+        LogUtil.debug("DebugRequestFragment", "performRequest: realBodyText = $realBodyText mediaType=$mediaType")
+        return realBodyText?.toRequestBody(mediaType?.toMediaTypeOrNull())
+    }
+
+    private fun parseHeader(headers: String): Headers {
+        val builder = Headers.Builder()
+        if (headers.contains("\n")) {
+            val lines = headers.split("\n")
+            lines.forEach {
+                val key = it.split(": ").getOrNull(0)
+                val value = it.split(": ").getOrNull(1)
+                if (key.isNullOrEmpty().not() && value != null) {
+                    builder.add(key!!, value)
+                }
+            }
+        } else {
+            val params = headers.split("&")
+            params.forEach {
+                val key = it.split("=").getOrNull(0)
+                val value = it.split("=").getOrNull(1)
+                if (key.isNullOrEmpty().not() && value != null) {
+                    builder.add(key!!, value)
+                }
+            }
+        }
+        return builder.build()
     }
 }
